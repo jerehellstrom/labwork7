61eded3604601a9d19cdaeb20311d3e5f87c2ad1
jerehellstrom <72606337+jerehellstrom@users.noreply.github.com>
2022-11-01 17:20:25 +0200

    initial commit

---
+++ labwork/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDgqh4L6BHoLAlTMAlPGna0U-MiJFI8-x4",
+    authDomain: "labwork4-b7734.firebaseapp.com",
+    projectId: "labwork4-b7734",
+    storageBucket: "labwork4-b7734.appspot.com",
+    messagingSenderId: "761628568576",
+    appId: "1:761628568576:web:1e518c4a414d42278c7ce5",
+    measurementId: "G-WKDRPGK361"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
